00df85f2fe5956a90418b45f4e878deec61121c9
Mahesh Kadam <mahesh.kadam@appian.com>
2025-02-25 01:40:25 +0530

    lms-0 : file structure

---
+++ src/main/java/java/LibraryManagementApp.java
@@ -0,0 +1,10 @@
+package java;
+
+public class LibraryManagementApp {
+
+
+//  whenever we load .class file, jvm look for main method with this signature to run(Note : load NOT INTIALIZE)
+  public static void main(String[] arg){
+    System.out.println("Not Implemented Yet");
+  }
+}
---
+++ src/main/java/java/com/library/enums/README.md
@@ -0,0 +1,4 @@
+├── enums/                     # Enum Definitions
+│   │   │   │   ├── UserRole.java          # Enum for user roles
+│   │   │   │   ├── BookCategory.java      # Enum for book genres
+│   │   │   │   ├── BorrowStatus.java      # Enum for borrow states
